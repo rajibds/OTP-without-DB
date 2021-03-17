c6eadbc9e5f3393ab65134a7276e3d55ea8d463c
rajibds <rajib.das.shuva@gmail.com>
2021-03-17 21:05:45 +0600

    add specs for verify action of otps_controller

--- spec/controllers/otps_controller_spec.rb
+++ spec/controllers/otps_controller_spec.rb
@@ -16,17 +16,54 @@ RSpec.describe OtpsController, type: :controller do
         encrypted_hash: encrypted_hash,
       }
     end
-    render_views
 
     before do
       allow(::OTPManagement::GenerateOTPWithEncryptedHash).to receive(:call).with(email: email).
         and_return otp_with_encrypted_hash
     end
 
-    it 'returns the list of courses in JSON' do
+    it 'returns a json of email and encrypted hash' do
       get :generate, params: { email: email, format: :json }
 
       expect(JSON.parse(response.body, symbolize_names: true)).to eq expected_response
+    end
+  end
+
+  describe 'GET #verify' do
+    let(:email) { 'test@test.com' }
+    let(:otp) { '123456' }
+    let(:encrypted_hash) { 'abcxyz' }
+    let(:verification_status_with_message) do
+      {
+        status: status,
+        message: message,
+      }
+    end
+
+    before do
+      allow(::OTPManagement::VerifyOTPUsingEncryptedHash).to receive(:call).
+        with(email: email, otp: otp, encrypted_hash: encrypted_hash).and_return verification_status_with_message
+    end
+
+    context 'successful verification' do
+      let(:status) { :success }
+      let(:message) { 'OTP Verification Successful' }
+
+      it 'returns the verification message as response' do
+        get :verify, params: { email: email, otp: otp, encrypted_hash: encrypted_hash, format: :json }
+        expect(JSON.parse(response.body, symbolize_names: true)).to eq({ message: message })
+      end
+    end
+
+    context 'unsuccessful verification' do
+      let(:status) { :unprocessable_entity }
+      let(:message) { 'OTP Verification Unsuccessful' }
+
+      it 'returns the verification message as response' do
+        get :verify, params: { email: email, otp: otp, encrypted_hash: encrypted_hash, format: :json }
+        expect(response.status).to eq 422
+        expect(JSON.parse(response.body, symbolize_names: true)).to eq({ message: message })
+      end
     end
   end
 end
